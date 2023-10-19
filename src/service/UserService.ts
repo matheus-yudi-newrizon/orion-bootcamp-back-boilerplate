e7702c70ee5331374f017e1e2b0de3701358c15c
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2023-10-19 16:36:58 -0300

    feat: adiciona campo confirmPassword no cadastro e documentação

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -3,9 +3,29 @@ import { MysqlDataSource } from '../config/database';
 import { User } from '../entity/User';
 
 export class UserService {
-  private static userRepository: Repository<User> = MysqlDataSource.getRepository(User);
+  private static userRepository: Repository<User> =
+    MysqlDataSource.getRepository(User);
 
-  public static async createUser(email: string, password: string): Promise<User> {
+  /**
+   * Creates a new user with the provided email and password. It ensures that
+   * the password and the confirmation password match before creating the user.
+   *
+   * @param email - The user's email.
+   * @param password - The user's password.
+   * @param confirmPassword - The password confirmation to ensure it matches the password.
+   *
+   * @returns A promise that resolves with the created user.
+   * @throws An error if the password and confirmation password do not match.
+   */
+  public static async createUser(
+    email: string,
+    password: string,
+    confirmPassword: string
+  ): Promise<User> {
+    if (password !== confirmPassword) {
+      throw new Error('Passwords do not match');
+    }
+
     const usuario = new User();
     usuario.email = email;
     usuario.password = password;
