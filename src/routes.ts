5813e7b91e735fec27f9b002fb6bd19d1b0e355c
Hebert Soares <hebertsoaresof@gmail.com>
2023-10-24 16:18:29 -0300

    Corrigir rotas para o método POST em UserController

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { HomeController } from './controller/HomeController';
-import UserController from 'controller/UserController';
+import { UserController } from './controller/UserController'
 
 const router = Router();
 
-router.get('/', new HomeController().hello);
 
-router.get('/user/signup', new UserController().signup);
+router.get('/signup', new UserController().signup);
+
+router.post('/signup', new UserController().signup);
 
 export default router;
