47c375aa08eeab39bb8b6d4424685211c5590f5f
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2023-10-24 01:20:59 -0300

    feat: Adiciona classe PasswordEncrypt para criptografia de senha

---
+++ src/security/PasswordEncrypt.ts
@@ -0,0 +1,26 @@
+// import * as bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
+
+export class PasswordEncrypt {
+  /**
+   * Hashes a plain text password and returns the hashed password.
+   *
+   * @param password - The plain text password to be hashed.
+   * @returns A promise that resolves with the hashed password.
+   */
+  public static async encrypt(password: string): Promise<string> {
+    const saltRounds = 10; // Número de rounds para a criptografia
+    return bcrypt.hash(password, saltRounds);
+  }
+
+  /**
+   * Compares a plain text password with a hashed password to check if they match.
+   *
+   * @param password - The plain text password to be compared.
+   * @param hashedPassword - The hashed password to compare with.
+   * @returns A promise that resolves with a boolean indicating if the passwords match.
+   */
+  public static async compare(password: string, hashedPassword: string): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+}
