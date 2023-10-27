1b56b402fa33046432a55ac9681142af7da0b762
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-10-26 21:01:01 -0300

    feat: implementação do teste da rota ping pong e configurações iniciais do jest

---
+++ src/test/ping.test.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import request from 'supertest';
+import routes from '../../routes';
+
+const app = express();
+app.use(routes);
+
+describe('GET /ping', () => {
+  it('should return 200 and pong', async () => {
+    const response = await request(app).get('/ping');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('pong');
+  });
+});
