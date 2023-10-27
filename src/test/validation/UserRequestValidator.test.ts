f63b4fc696216074d705a1dbfa56db4e0f80c70d
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2023-10-27 02:05:37 -0300

    test: Adiciona testes para a classe UserRequestValidator

---
+++ src/test/validation/UserRequestValidator.test.ts
@@ -0,0 +1,56 @@
+import { EmailNotValidException, PasswordMismatchException, PasswordNotValidException } from '../../exception';
+import { UserRequestValidator } from '../../validation/UserRequestValidator';
+
+describe('UserRequestValidator', () => {
+  describe('validateUserEmail', () => {
+    it('should not throw an exception for a valid email', () => {
+      expect(() => UserRequestValidator.validateUserEmail('user@example.com')).not.toThrow();
+    });
+
+    it('should throw EmailNotValidException for an invalid email', () => {
+      expect(() => UserRequestValidator.validateUserEmail('@example.com')).toThrow(EmailNotValidException);
+    });
+
+    it('should throw EmailNotValidException for an invalid email 2', () => {
+      expect(() => UserRequestValidator.validateUserEmail('userexample.com')).toThrow(EmailNotValidException);
+    });
+
+    it('should throw EmailNotValidException for an invalid email 3', () => {
+      expect(() => UserRequestValidator.validateUserEmail('user@.com')).toThrow(EmailNotValidException);
+    });
+
+    it('should throw EmailNotValidException for an invalid email 4', () => {
+      expect(() => UserRequestValidator.validateUserEmail('user@examplecom')).toThrow(EmailNotValidException);
+    });
+  });
+
+  describe('validateUserPassword', () => {
+    it('should not throw an exception for a valid password and matching confirmation', () => {
+      expect(() => UserRequestValidator.validateUserPassword('P@ssw0rd', 'P@ssw0rd')).not.toThrow();
+    });
+
+    it('should throw PasswordMismatchException for non-matching passwords', () => {
+      expect(() => UserRequestValidator.validateUserPassword('P@ssw0rd', 'DifferentP@ssw0rd')).toThrow(PasswordMismatchException);
+    });
+
+    it('should throw PasswordNotValidException for an invalid password', () => {
+      expect(() => UserRequestValidator.validateUserPassword('invalid', 'invalid')).toThrow(PasswordNotValidException);
+    });
+
+    it('should throw PasswordNotValidException for a password without an uppercase letter', () => {
+      expect(() => UserRequestValidator.validateUserPassword('p@ssw0rd', 'p@ssw0rd')).toThrow(PasswordNotValidException);
+    });
+
+    it('should throw PasswordNotValidException for a password without a lowercase letter', () => {
+      expect(() => UserRequestValidator.validateUserPassword('P@SSW0RD', 'P@SSW0RD')).toThrow(PasswordNotValidException);
+    });
+
+    it('should throw PasswordNotValidException for a password without a special character', () => {
+      expect(() => UserRequestValidator.validateUserPassword('Passw0rd', 'Passw0rd')).toThrow(PasswordNotValidException);
+    });
+
+    it('should throw PasswordNotValidException for a password without a digit', () => {
+      expect(() => UserRequestValidator.validateUserPassword('P@ssword', 'P@ssword')).toThrow(PasswordNotValidException);
+    });
+  });
+});
