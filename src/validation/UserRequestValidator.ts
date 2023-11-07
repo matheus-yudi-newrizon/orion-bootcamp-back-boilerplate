d41fe0e133438f0187a3cb2df882b3823d087912
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-11-07 13:13:18 -0300

    fix: correção da regexp de validação de email

--- src/validation/UserRequestValidator.ts
+++ src/validation/UserRequestValidator.ts
@@ -12,7 +12,7 @@ export class UserRequestValidator {
    */
   public static validateUserEmail(email: string): void {
     const emailPattern =
-      /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 
     if (!emailPattern.test(email)) {
       throw new EmailNotValidException();
