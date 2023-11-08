7fd5e85cf92f3a01fc28f7ec66d809ddf1612cf7
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-11-08 18:06:47 -0300

    style: mudança no nome de variável, ajustes nos comentários e remoção de chaves de blocos if

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -14,46 +14,43 @@ export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
   /**
-   * Creates a new user with the provided userPostRequest.
+   * Creates a new user with the provided user data.
    *
-   * @param userPostRequest - The user data including email and password.
+   * @param userDTO - The user data including email and password.
    *
    * @returns A promise that resolves with the created user.
    * @throws {UserAlreadyExistsException} if the user already exists.
    * @throws {DatabaseOperationFailException} if there is a database operation failure.
    */
-  public async createUser(userPostRequest: IUserPostRequest): Promise<UserResponseDTO> {
-    const userByEmail: User = await this.userRepository.getByEmail(userPostRequest.email);
-    if (userByEmail) throw new UserAlreadyExistsException(userPostRequest.email);
+  public async createUser(userDTO: IUserPostRequest): Promise<UserResponseDTO> {
+    const userByEmail: User = await this.userRepository.getByEmail(userDTO.email);
+    if (userByEmail) throw new UserAlreadyExistsException(userDTO.email);
 
-    const passwordEncrypted: string = await PasswordEncrypt.encrypt(userPostRequest.password);
-    userPostRequest.password = passwordEncrypted;
+    const passwordEncrypted: string = await PasswordEncrypt.encrypt(userDTO.password);
+    userDTO.password = passwordEncrypted;
 
-    const user: User = this.userRepository.create(userPostRequest);
+    const user: User = this.userRepository.create(userDTO);
     await this.userRepository.save(user);
 
     return new UserResponseDTO(user);
   }
 
   /**
-   * Performs user login using the provided user data.
+   * Performs user login with the provided user data.
    *
    * @param userDTO - The user data including email and password.
    * @param rememberMe - A flag indicating whether the session should be remembered.
    *
    * @returns A promise that resolves with the login response, including an authentication token.
    * @throws {AuthenticationFailedException} if the email or password is incorrect.
+   * @throws {DatabaseOperationFailException} if there is a database operation failure.
    */
   public async login(userDTO: IUserPostRequest, rememberMe: boolean): Promise<LoginResponseDTO> {
     const user: User = await this.userRepository.getByEmail(userDTO.email);
-    if (!user) {
-      throw new AuthenticationFailedException();
-    }
+    if (!user) throw new AuthenticationFailedException();
 
     const validPassword = await PasswordEncrypt.compare(userDTO.password, user.password);
-    if (!validPassword) {
-      throw new AuthenticationFailedException();
-    }
+    if (!validPassword) throw new AuthenticationFailedException();
 
     const expiresIn = rememberMe ? undefined : '5h';
     const token = JwtService.generateToken({ id: user.id, email: user.email }, expiresIn);
