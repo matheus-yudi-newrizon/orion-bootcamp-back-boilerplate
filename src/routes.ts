3cf79226bf6043ec3b10b3885d4a16b2c3d740f3
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-11-08 17:58:42 -0300

    style: alteração no routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -9,7 +9,6 @@ const userController: UserController = Container.get(UserController);
 router.get('/ping', (_req, res) => res.send('pong'));
 
 router.post('/signup', (req, res) => userController.signup(req, res));
-
 router.post('/login', (req, res) => userController.login(req, res));
 
 export default router;
