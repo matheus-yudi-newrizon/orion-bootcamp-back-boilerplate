e73cc94da6f0be1cde37557a2288a21d79d70b0f
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-11-09 22:33:05 -0300

    test: adição dos testes das classes de security PasswordEncrypt e JwtService

--- __test__/security/PasswordEncrypt.test.ts
+++ __test__/security/PasswordEncrypt.test.ts
@@ -1,39 +1,45 @@
 import bcrypt from 'bcrypt';
 import { PasswordEncrypt } from '../../src/security/PasswordEncrypt';
+import { Generate } from '../mocks/Generate';
+
+const generate = new Generate();
 
 describe('PasswordEncrypt', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
   describe('encrypt', () => {
-    it('should encrypt password', async () => {
-      const password = 'myPassword';
+    it('should encrypt the provided password', async () => {
+      const { password } = generate.userPostRequest();
+      const hashed = generate.hashedPassword();
+
+      const spyHash = jest.spyOn(bcrypt, 'hash').mockImplementation(() => new Promise(resolve => resolve(hashed)));
 
       const result = await PasswordEncrypt.encrypt(password);
-
-      await expect(bcrypt.compare(password, result)).resolves.toBe(true);
+      expect(result).toBe(hashed);
+      expect(spyHash).toHaveBeenCalledWith(password, 10);
     });
   });
 
   describe('compare', () => {
-    it('should return true when passwords match', async () => {
-      const password = 'myPassword';
-      const hashedPassword = await PasswordEncrypt.encrypt(password);
+    it('should return true if passwords match', async () => {
+      const { password } = generate.userPostRequest();
+      const hashed = generate.hashedPassword();
 
-      const result = await PasswordEncrypt.compare(password, hashedPassword);
+      const spyCompare = jest.spyOn(bcrypt, 'compare').mockImplementation(() => new Promise(resolve => resolve(true)));
 
+      const result = await PasswordEncrypt.compare(password, hashed);
       expect(result).toBe(true);
+      expect(spyCompare).toHaveBeenCalledWith(password, hashed);
     });
 
-    it('should return false when passwords match', async () => {
-      const password = 'myPassword';
-      const hashedPassword = await PasswordEncrypt.encrypt(password);
+    it('should return false if passwords do not match', async () => {
+      let { password } = generate.userPostRequest();
+      password += 'foo.bar';
+      const hashed = generate.hashedPassword();
 
-      const otherPassword = 'otherPassword';
-      const result = await PasswordEncrypt.compare(otherPassword, hashedPassword);
+      const spyCompare = jest.spyOn(bcrypt, 'compare').mockImplementation(() => new Promise(resolve => resolve(false)));
 
+      const result = await PasswordEncrypt.compare(password, hashed);
       expect(result).toBe(false);
+      expect(spyCompare).toHaveBeenCalledWith(password, hashed);
     });
   });
 });
