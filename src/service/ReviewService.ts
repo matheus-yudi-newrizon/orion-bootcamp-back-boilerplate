875d6dd7b25bd5bff8298518706b717d6e520597
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-11-21 19:12:59 -0300

    feat: melhoria da lógica para verificar se uma review já foi utilizada

--- src/service/ReviewService.ts
+++ src/service/ReviewService.ts
@@ -4,8 +4,7 @@ import { Game } from '../entity/Game';
 import { GameReview } from '../entity/GameReview';
 import { Review } from '../entity/Review';
 import { User } from '../entity/User';
-import { GameNotFoundException } from '../exception/GameNotFoundException';
-import { UserNotFoundException } from '../exception/UserNotFoundException';
+import { EntityNotFoundException } from '../exception/EntityNotFoundException';
 import { GameRepository } from '../repository/GameRepository';
 import { GameReviewRepository } from '../repository/GameReviewRepository';
 import { ReviewRepository } from '../repository/ReviewRepository';
@@ -26,32 +25,23 @@ export class ReviewService {
    * @param userId - The id of the user that requested a new review.
    *
    * @returns A promise that resolves with a review.
-   * @throws {UserNotFoundException} if the user was not found in database.
+   * @throws {EntityNotFoundException} if the user was not found in database.
    * @throws {GameNotFoundException} if the active game was not found in database.
    * @throws {DatabaseOperationFailException} if there is a database operation failure.
    */
   public async getReview(userId: number): Promise<ReviewDTO> {
     const user: User = await this.userRepository.getById(userId);
-    if (!user) throw new UserNotFoundException();
+    if (!user) throw new EntityNotFoundException('user');
 
     const game: Game = await this.gameRepository.getActiveGameByUser(user);
-    if (!game) throw new GameNotFoundException();
+    if (!game) throw new EntityNotFoundException('game');
 
     const gameReviews: GameReview[] = await this.gameReviewRepository.getByGame(game);
-    let isRepeated: boolean;
     let review: Review;
 
     do {
-      isRepeated = false;
       review = await this.reviewRepository.getRandomReview();
-
-      for (const gameReview of gameReviews) {
-        if (gameReview.review.id === review.id) {
-          isRepeated = true;
-          break;
-        }
-      }
-    } while (isRepeated);
+    } while (gameReviews.some(gameReview => gameReview.review.id === review.id));
 
     return new ReviewDTO(review);
   }
