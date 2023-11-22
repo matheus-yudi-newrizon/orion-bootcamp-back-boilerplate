a7d4d1630f6187ffc41acd3f459077d25272e1c3
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2023-11-21 22:11:00 -0300

    style: muda a tradução para o inglês

--- src/migrations/1700611974260-MoviesConsume.ts
+++ src/migrations/1700611974260-MoviesConsume.ts
@@ -4,10 +4,10 @@ import { Movie } from '../entity/Movie';
 
 export class MoviesConsume1700611974260 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const dados: string = await fs.readFile('/app/movies.json', 'utf-8');
-    const dadosFormatados: Movie[] = JSON.parse(dados);
+    const data: string = await fs.readFile('/app/movies.json', 'utf-8');
+    const movies: Movie[] = JSON.parse(data);
 
-    await queryRunner.manager.save('Movie', dadosFormatados);
+    await queryRunner.manager.save('Movie', movies);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
