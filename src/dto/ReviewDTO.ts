c8530b1665d71641330a07cea2011f03db92a82a
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-11-30 13:37:03 -0300

    feat: campos adicionais em MovieDTO e ReviewDTO

--- src/dto/ReviewDTO.ts
+++ src/dto/ReviewDTO.ts
@@ -3,9 +3,11 @@ import { Review } from '../entity/Review';
 export class ReviewDTO {
   id: string;
   text: string;
+  author: string;
 
   constructor(review: Review) {
     this.id = review.id;
     this.text = review.text;
+    this.author = review.author;
   }
 }
