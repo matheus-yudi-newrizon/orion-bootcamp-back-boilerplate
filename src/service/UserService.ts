8586dd891ee273351662d928c0c111ed074ddc2f
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-11-29 22:56:53 -0300

    feat: adaptação do signup para enviar o email de confirmação

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,14 +1,20 @@
+import * as crypto from 'crypto';
 import { Service } from 'typedi';
-import { UserResponseDTO } from '../dto/UserResponseDTO';
+import { Token } from '../entity/Token';
 import { User } from '../entity/User';
-import { UserAlreadyExistsException } from '../exception';
+import { OperationFailException } from '../exception/OperationFailException';
 import { IUserPostRequest } from '../interface/IUserPostRequest';
+import { TokenRepository } from '../repository/TokenRepository';
 import { UserRepository } from '../repository/UserRepository';
 import { PasswordEncrypt } from '../security/PasswordEncrypt';
+import { EmailService } from '../utils/EmailService';
 
 @Service()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly tokenRepository: TokenRepository
+  ) {}
 
   /**
    * Creates a new user with the provided user data.
@@ -18,17 +24,40 @@ export class UserService {
    * @returns A promise that resolves with the created user.
    * @throws {UserAlreadyExistsException} if the user already exists.
    * @throws {DatabaseOperationFailException} if there is a database operation failure.
+   * @throws {SendEmailFailException} if there is a failure sending email.
    */
-  public async createUser(userDTO: IUserPostRequest): Promise<UserResponseDTO> {
+  public async createUser(userDTO: IUserPostRequest): Promise<void> {
     const userByEmail: User = await this.userRepository.getByEmail(userDTO.email);
-    if (userByEmail) throw new UserAlreadyExistsException(userDTO.email);
+    if (userByEmail?.isActive) throw new OperationFailException('It was not possible to create user.');
 
-    const passwordEncrypted: string = await PasswordEncrypt.encrypt(userDTO.password);
-    userDTO.password = passwordEncrypted;
+    if (userByEmail) {
+      const oldToken: Token = await this.tokenRepository.findById(userByEmail.id);
+
+      if (oldToken) {
+        const expiresIn: number = oldToken.createdAt.getTime() + 1800000;
+        const currentTime: number = new Date().getTime();
+
+        if (expiresIn > currentTime) throw new OperationFailException('Check your email address.');
+
+        await this.tokenRepository.deleteById(userByEmail.id);
+      }
+    } else {
+      const passwordEncrypted: string = await PasswordEncrypt.encrypt(userDTO.password);
+      userDTO.password = passwordEncrypted;
+    }
+
+    const confirmToken: string = crypto.randomBytes(32).toString('hex');
+    const hash: string = await PasswordEncrypt.encrypt(confirmToken);
 
     const user: User = this.userRepository.create(userDTO);
     await this.userRepository.save(user);
 
-    return new UserResponseDTO(user);
+    const token: Token = this.tokenRepository.create({ user, token: hash });
+    await this.tokenRepository.save(token);
+
+    const username: string = user.email.slice(0, user.email.indexOf('@'));
+    const link: string = `${process.env.CLIENT_URL}/auth/confirm-email?token=${confirmToken}&id=${user.id}`;
+
+    await EmailService.sendEmail(user.email, 'ReviewReveal: Confirm your email', { username, link }, './template/ConfirmEmail.hbs');
   }
 }
