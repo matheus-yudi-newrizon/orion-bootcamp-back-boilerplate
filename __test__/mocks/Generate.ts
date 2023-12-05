bdd284dd48d316e6e4f3c3d1157b177c63878fb4
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-12-05 15:33:00 -0300

    test: ajustes e adições nos métodos de mock

--- __test__/mocks/Generate.ts
+++ __test__/mocks/Generate.ts
@@ -1,6 +1,6 @@
-import { GameResponseDTO, LoginResponseDTO, UserResponseDTO } from '../../src/dto';
-import { Game, Token, User } from '../../src/entity';
-import { IUserPostRequest } from '../../src/interface';
+import { GameResponseDTO, GameReviewResponseDTO, LoginResponseDTO, UserResponseDTO } from '../../src/dto';
+import { Game, GameReview, Movie, Review, Token, User } from '../../src/entity';
+import { IGameAnswerRequest, IUserPostRequest } from '../../src/interface';
 
 /**
  * Class for generating mock data for tests.
@@ -16,6 +16,22 @@ export class Generate {
       email: 'orion@email.com',
       password: '12345678aA!',
       confirmPassword: '12345678aA!'
+    };
+
+    return input;
+  }
+
+  /**
+   * Generates a mock of confirm email input.
+   *
+   * @returns An object with the mocked data.
+   */
+  public confirmEmailInput() {
+    const token: Token = this.tokenData();
+
+    const input = {
+      id: token.id,
+      token: token.token
     };
 
     return input;
@@ -79,6 +95,20 @@ export class Generate {
   }
 
   /**
+   * Generates a mock of send answer request.
+   *
+   * @returns A IGameAnswerRequest with the mocked data.
+   */
+  public gameAnswerRequest(): IGameAnswerRequest {
+    const gameAnswerRequest: IGameAnswerRequest = {
+      reviewId: '4fcb0d2a19c2955e3c000e35',
+      answer: 22
+    };
+
+    return gameAnswerRequest;
+  }
+
+  /**
    * Generates a mock of user data.
    *
    * @param isActive - A flag indicating whether the user is active or not. Default is `true`.
@@ -121,11 +151,9 @@ export class Generate {
    * @returns A Token with the mocked data.
    */
   public tokenData(): Token {
-    const user = this.userData();
-
     const token: Token = {
       id: 1,
-      user,
+      user: null,
       token: 'b55ea11ca19f9674b6a5d60d6c098c6d511e8bdf7de9571a8dfd5e6b56e7ec22',
       createdAt: new Date()
     };
@@ -153,7 +181,7 @@ export class Generate {
    */
   public loginResponse(): LoginResponseDTO {
     const jwt: string = this.encodedJwt();
-    const game: Game = this.newGame();
+    const game: Game = this.gameData();
     const user: User = this.userData();
     const loginReponse: LoginResponseDTO = new LoginResponseDTO(jwt, new GameResponseDTO(game, user));
 
@@ -167,9 +195,21 @@ export class Generate {
    */
   public encodedJwt(): string {
     const jwt: string =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJvcmlvbkBlbWFpbC5jb20ifQ.q6QO9j1G4VlfKx2pPqH5uJ5Egrdv6c6--LjGBI4vliQ';
 
     return jwt;
+  }
+
+  /**
+   * Generates a mock of encoded JWT data.
+   *
+   * @returns A string with the mocked data.
+   */
+  public cookieData(): string {
+    const jwt: string = this.encodedJwt();
+    const cookie: string = `refreshToken=${jwt}; Path=/; HttpOnly; SameSite=Strict; Domain=localhost`;
+
+    return cookie;
   }
 
   /**
@@ -186,16 +226,19 @@ export class Generate {
   /**
    * Generates a mock of new game.
    *
+   * @param score - A number indicating the game score. Default is `0`.
+   * @param combo - A number indicating the current combo. Default is `0`.
+   *
    * @returns A Game with the mocked data.
    */
-  public newGame(): Game {
+  public gameData(score: number = 0, combo: number = 0): Game {
     const user: User = this.userData();
     const game: Game = {
       id: 1,
       user,
       lives: 2,
-      score: 0,
-      combo: 0,
+      score,
+      combo,
       isActive: true,
       createdAt: new Date(),
       updatedAt: new Date(),
@@ -204,5 +247,86 @@ export class Generate {
     };
 
     return game;
+  }
+
+  /**
+   * Generates a mock of review.
+   *
+   * @returns A Review with the mocked data.
+   */
+  public reviewData(): Review {
+    const movie: Movie = this.movieData();
+    const review: Review = {
+      id: '5424a49ec3a3681eca002c23',
+      text: 'Ah, but you have heard of me. The crew of the Black Pearl are cursed by something most unimaginable, the only way to lift the curse is to return a lost Aztec coin to its treasure chest home. That is a shame, but at the very least we still have this wonderful picture to go back to time and time again, to lift you up when one is down or to keep one happy when one is already in that happy place. The Curse Of The Black Pearl is a joy from start to finish. 9/10',
+      author: 'John Chard',
+      movie,
+      gameReviews: []
+    };
+
+    return review;
+  }
+
+  /**
+   * Generates a mock of movie.
+   *
+   * @returns A Movie with the mocked data.
+   */
+  public movieData(): Movie {
+    const movie: Movie = {
+      id: 22,
+      imdbId: 'tt0325980',
+      title: 'Pirates of the Caribbean: The Curse of the Black Pearl',
+      posterPath: '/z8onk7LV9Mmw6zKz4hT6pzzvmvl.jpg',
+      releaseDate: new Date('2003-07-09 00:00:00'),
+      reviews: []
+    };
+
+    return movie;
+  }
+
+  /**
+   * Generates a mock of game review answered.
+   *
+   * @returns A GameReview with the mocked data.
+   */
+  public gameReviewData(): GameReview {
+    const game: Game = this.gameData();
+    const review: Review = this.reviewData();
+
+    const gameReview: GameReview = {
+      id: 1,
+      game,
+      review,
+      answer: null,
+      isCorrect: null,
+      createdAt: new Date()
+    };
+    gameReview.game.currentGameReview = gameReview;
+
+    return gameReview;
+  }
+
+  /**
+   * Generates a mock of game response DTO.
+   *
+   * @returns A GameResponseDTO with the mocked data.
+   */
+  public gameResponse(): GameResponseDTO {
+    const game: Game = this.gameData();
+
+    return new GameResponseDTO(game, game.user);
+  }
+
+  /**
+   * Generates a mock of game review response DTO.
+   *
+   * @returns A GameReviewResponseDTO with the mocked data.
+   */
+  gameReviewResponse(): GameReviewResponseDTO {
+    const gameReview: GameReview = this.gameReviewData();
+    const game: GameResponseDTO = this.gameResponse();
+
+    return new GameReviewResponseDTO(gameReview, game);
   }
 }
