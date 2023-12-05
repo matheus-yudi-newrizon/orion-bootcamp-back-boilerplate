6858b17dd1ca9f0a35379374623aa10fc166a7fc
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2023-12-04 22:23:59 -0300

    fix: remove id do reviewDTO

--- src/dto/ReviewDTO.ts
+++ src/dto/ReviewDTO.ts
@@ -1,12 +1,10 @@
 import { Review } from '../entity';
 
 export class ReviewDTO {
-  id: string;
   text: string;
   author: string;
 
   constructor(review: Review) {
-    this.id = review.id;
     this.text = review.text;
     this.author = review.author;
   }
