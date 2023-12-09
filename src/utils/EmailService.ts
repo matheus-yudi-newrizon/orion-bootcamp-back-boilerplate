7ecc9aaddd12c82f513ba50fb367951f06ec76ad
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-12-08 23:57:08 -0300

    feat: melhoria do EmailService

--- src/utils/EmailService.ts
+++ src/utils/EmailService.ts
@@ -20,34 +20,72 @@ export class EmailService {
    * @returns A promise that resolves with void.
    */
   public static async sendEmail(email: string, subject: string, payload: unknown, template: string): Promise<void> {
-    const transporter: Transporter = nodemailer.createTransport({
-      service: process.env.EMAIL_SERVICE,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
-      }
-    });
+    const transporters: Transporter[] = [
+      nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD
+        }
+      }),
+      nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE_2,
+        auth: {
+          user: process.env.EMAIL_USERNAME_2,
+          pass: process.env.EMAIL_PASSWORD_2
+        }
+      }),
+      nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE_3,
+        auth: {
+          user: process.env.EMAIL_USERNAME_3,
+          pass: process.env.EMAIL_PASSWORD_3
+        }
+      })
+    ];
 
     const source: string = fs.readFileSync(path.join(__dirname, template), 'utf8');
     const compiledTemplate = handlebars.compile(source);
 
-    const options = {
-      from: process.env.EMAIL_USERNAME,
-      to: email,
-      subject: subject,
-      html: compiledTemplate(payload)
-    };
+    const options = [
+      {
+        from: process.env.EMAIL_USERNAME,
+        to: email,
+        subject: subject,
+        html: compiledTemplate(payload)
+      },
+      {
+        from: process.env.EMAIL_USERNAME_2,
+        to: email,
+        subject: subject,
+        html: compiledTemplate(payload)
+      },
+      {
+        from: process.env.EMAIL_USERNAME_3,
+        to: email,
+        subject: subject,
+        html: compiledTemplate(payload)
+      }
+    ];
 
-    transporter.verify(function (error: Error) {
-      if (error) console.log(error.message);
+    transporters[0].verify().catch(error => {
+      console.log(`${process.env.EMAIL_USERNAME} - ${error.message}`);
+      transporters[1].verify().catch(error => {
+        console.log(`${process.env.EMAIL_USERNAME_2} - ${error.message}`);
+        transporters[2].verify().catch(error => {
+          console.log(`${process.env.EMAIL_USERNAME_3} - ${error.message}`);
+        });
+      });
     });
 
-    transporter.sendMail(options, (error: Error, info: SentMessageInfo) => {
-      if (error) {
-        console.log(error.message);
-      } else {
-        console.log('Message sent: ' + info.messageId);
-      }
+    transporters[0].sendMail(options[0]).catch(error => {
+      console.log(`${process.env.EMAIL_USERNAME} - ${error.message}`);
+      transporters[1].sendMail(options[1]).catch(error => {
+        console.log(`${process.env.EMAIL_USERNAME_2} - ${error.message}`);
+        transporters[2].sendMail(options[2]).catch(error => {
+          console.log(`${process.env.EMAIL_USERNAME_3} - ${error.message}`);
+        });
+      });
     });
   }
 }
